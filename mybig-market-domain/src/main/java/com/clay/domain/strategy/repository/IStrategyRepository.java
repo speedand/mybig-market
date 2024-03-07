cfa4f3ed24defbfbe0dcc69474fe7ba020eb0a0b
speedand <1366255931@qq.com>
2024-03-07 12:00:34 +0800

    配置Redis

---
+++ mybig-market-domain/src/main/java/com/clay/domain/strategy/repository/IStrategyRepository.java
@@ -0,0 +1,21 @@
+package com.clay.domain.strategy.repository;
+
+import com.clay.domain.strategy.model.entity.StrategyAwardEntity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @description 策略服务仓储接口
+ */
+public interface IStrategyRepository {
+
+    List<StrategyAwardEntity> queryStrategyAwardList(Long strategyId);
+
+    void storeStrategyAwardSearchRateTable(Long strategyId, Integer rateRange, Map<Integer, Integer> strategyAwardSearchRateTable);
+
+    Integer getStrategyAwardAssemble(Long strategyId, Integer rateKey);
+
+    int getRateRange(Long strategyId);
+
+}
